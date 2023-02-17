d316ee79984f5547f44d1e123cd71d7e63e8971d
myUsername <myemail@gmail.com>
2023-02-17 15:00:03 -0800

    Print alphabet c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line, expect q and e
+ * Retun: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+	return (0);
+}
