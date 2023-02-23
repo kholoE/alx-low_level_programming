f958e2cb33d64b1ac94f569d7af12d7882b3d2a2
myUsername <myemail@gmail.com>
2023-02-23 03:40:24 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,9 +5,10 @@
  * @c: char to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
+
 int _isdigit(int c)
 {
-	if ((c >= 0) && (c <= 9))
+	if ((c >= '0') && (c <= '9'))
 		return (1);
 	else
 		return (0);
