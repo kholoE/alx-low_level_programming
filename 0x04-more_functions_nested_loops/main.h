561b75837aa2af50845ea472e2ca01c3633c0957
myUsername <myemail@gmail.com>
2023-02-23 05:37:52 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,6 +12,9 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+long int biggestFactor(long int a);
+int isPrime(long int n);
+int numDigits(int n);
 void print_number(int n);
 
 #endif
