9da89b90fb0cc71cdb0c55c42b1de70ea61d6095
myUsername <myemail@gmail.com>
2023-03-08 12:08:14 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,3 +15,16 @@ int _sqrt(int n, int i)
 		return (i);
 	return (_sqrt(n, i + 1));
 }
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number.
+ * @n: number to be evaluated
+ *
+ * Return: square root result
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 0));
+}
